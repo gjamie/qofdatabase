194b8e8f2c96a3c987e331dbf10bf834904618b8
Gavin Jamie <qofdatabase@gmail.com>
2013-02-15 22:40:23 +0000

    Settings updated

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'qof',                      # Or path to database file if using sqlite3.
         'USER': 'gavin',                      # Not used with sqlite3.
-        'PASSWORD': 'spider',                  # Not used with sqlite3.
+        'PASSWORD': 'xxxx',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
