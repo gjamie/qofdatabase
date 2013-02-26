30889cd7548b2d98fa85cbcc9fbc7662eba9d02e
David Miller <david@deadpansincerity.com>
2013-02-26 10:56:32 +0000

    Tidying - don't commit bytecode, minor tweaks to urls.py

--- urls.py
+++ urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls.defaults import *
-from django.contrib import admin; admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('qofdb.views',
                        (url(r'^faq/', include('faq.urls'))),
-                       (url(r'^admin/', include(admin.site.urls))),
                        (r'^browse/(?P<orgcode>\w{1,6})$','browse'),
                        (r'^browse/(?P<orgcode>\w{1,6})/(?P<year>\d{1,2})','browse'),
                        (r'^browse/(?P<orgcode>\w{1,6})/(?P<area>[\w ]+)/(?P<year>\d{1,2})','browse'),
@@ -19,12 +16,12 @@ urlpatterns = patterns('qofdb.views',
                        (r'^pct.php','translate'),
                        (r'^download','download'),
                        (r'^$','browse',{'orgcode':'UK'}),
-                       # Example:
+
+                       (url(r'^admin/', include(admin.site.urls))),
+                       (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                      # Example:
     # (r'^qof/', include('qof.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
 )
